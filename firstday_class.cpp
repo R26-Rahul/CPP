0eb3a624ee5a6658137b46de5e788af7d0c18254
Rahul Kumar <109171650+R26-Rahul@users.noreply.github.com>
2025-04-24 23:26:12 +0530

    First day c++ class

---
+++ firstday_class.cpp
@@ -0,0 +1,15 @@
+// First day of C++ Class
+
+#include<iostream> // is a header file library that lets us work with input and output objects, such as cout (used in line 5). Header files add functionality to C++ programs.
+using namespace std; // means that we can use names for objects and variables from the standard library.
+int main() //This is called a function.
+{
+    //Any code inside its curly brackets {} will be executed.
+
+cout <<"rahl"; // (pronounced "see-out") is an object used together with the insertion operator (<<) to output/print text. In our example, it will output "Hello World!".
+// Note: C++ is case-sensitive: "cout" and "Cout" has different meaning.
+// Note: Every C++ statement ends with a semicolon ;
+
+std::cout <<"Rahul babat"; //  line can be omitted and replaced with the "using namespace std;" keyword, followed by the 'std::' operator for some objects.
+return 0; // ends the main function.
+}// Do not forget to add the closing curly bracket " } " to actually end the main function.
