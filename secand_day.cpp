08e146516325ba837a905200273e43ca3b633708
Rahul Kumar <109171650+R26-Rahul@users.noreply.github.com>
2025-04-25 11:15:45 +0530

    Day2 C++

---
+++ secand_day.cpp
@@ -0,0 +1,22 @@
+// create function
+// " using namespace std; replace with the std::
+//C++ Output (Print Text) : cout<< "text";
+// C++ Output Numbers : cout << 6; , cout<< 8+9;
+// To insert a new line in your output, you can use the "\n" character: cout<< "rahul \n rai";
+// You can also use another << operator and place the \n character after the text,: <<"\n";
+// Two \n characters after each other will create a blank line: "\n \n"
+// Another way to insert a new line, is with the "endl" manipulator: cout << "hellow " endl;
+// create horizontal tab "\t"
+// insert backshlash using "\\"
+// insert duble qoute character '\"'
+// online comments "//" and Multi-line comments start with /* and ends with */.
+#include<iostream>
+using namespace std;
+int main(){
+// cout << "rahul\n rai" << "\n \n";
+// cout << 4;
+// cout << "hello "<< endl; // its means end this line use next line to write somthing
+// cout << "hello \t create horizontal tab \\ insert backshlash \" creating duble qoute";
+
+return 0;
+}
